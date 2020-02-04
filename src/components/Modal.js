f9cd5845852f3dbd1bfc8e092255b4113be7f954
Lukas Sorensen <lsorensen@sportsdigita.com>
2020-02-03 19:45:30 -0600

    random default modal pos

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,26 +4,40 @@ import React from 'react';
 import Draggable from 'react-draggable';
 import { FormattedMessage } from 'react-intl';
 export default class Modal extends React.Component {
+	onModalClose() {
+		this.props.handleOnModalClose();
+	}
+
+	focusModal() {
+		this.props.handleFocusModal();
+    }
     
-    onModalClose() {
-        this.props.handleOnModalClose();
+    getRandomPosition() {
+        var maxY = 40;
+        var maxX = 150;
+        
+        var pos = {};
+        pos.y = -maxY + Math.random() * (maxY*2);
+        pos.x = -maxX + Math.random() * (maxX*2);
+        return pos;
     }
 
-    focusModal() {
-        this.props.handleFocusModal();
-    }
-    
 	render() {
 		return (
-            <Draggable onMouseDown={this.focusModal.bind(this)} handle=".modal-header">
-			<div className={`modal ${this.props.hidden ? 'hidden' : 'visible'}`} style={{zIndex: this.props.zIndex}}>
-				<div className="modal-header">
-					<div className="modal-close" onClick={this.onModalClose.bind(this)}>&times;</div>
-                    <FormattedMessage id={this.props.title} />
+			<Draggable onMouseDown={this.focusModal.bind(this)} handle=".modal-header" defaultPosition={this.getRandomPosition()}>
+				<div
+					className={`modal ${this.props.hidden ? 'hidden' : 'visible'}`}
+					style={{ zIndex: this.props.zIndex }}
+				>
+					<div className="modal-header">
+						<div className="modal-close" onClick={this.onModalClose.bind(this)}>
+							&times;
+						</div>
+						<FormattedMessage id={this.props.title} />
+					</div>
+					<div className="modal-body">{this.props.children}</div>
 				</div>
-				<div className="modal-body">{this.props.children}</div>
-			</div>
-            </Draggable>
+			</Draggable>
 		);
 	}
 }
