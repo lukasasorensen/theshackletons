344f085674e924c6d8b273454579f0c62ced85ee
Lukas Sorensen <lsorensen@sportsdigita.com>
2020-03-28 16:37:21 -0500

    add loader gif

--- src/components/Processing.js
+++ src/components/Processing.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
+import loadingGif from '../assets/loading.gif';
 import "../styles/Processing.scss";
 export default class Processing extends Component {
 	render() {
 		return (
 			<div className="processing-container">
+                <div className="loading-gif-container">
+                    <img src={loadingGif} alt="loading..."/>
+                </div>
                 <h2 className="processing-message">
                     <FormattedMessage id={this.props.message && this.props.message.length ? this.props.message : 'processing.defaultMessage' }/>
                 </h2>
