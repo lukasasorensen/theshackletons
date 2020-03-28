e9b65896d6875cccea30a6bef6a1a953dba14aae
Lukas Sorensen <lsorensen@sportsdigita.com>
2020-03-28 17:32:41 -0500

    Updates

---
+++ precache-manifest.67e861dd7a088328983058f95fa6ac91.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7338bb2f7998646e711166c0e094bfb",
+    "url": "/shackletons/index.html"
+  },
+  {
+    "revision": "fd9ecfeee8ee0b17f952",
+    "url": "/shackletons/static/css/3.70d6c955.chunk.css"
+  },
+  {
+    "revision": "4d2f5309c156143fdedd",
+    "url": "/shackletons/static/css/4.9a9c5ec0.chunk.css"
+  },
+  {
+    "revision": "d0ab2d9dd0709d6709ed",
+    "url": "/shackletons/static/css/5.d9493372.chunk.css"
+  },
+  {
+    "revision": "91eeb61308f2a3b40a55",
+    "url": "/shackletons/static/css/6.0d7b810a.chunk.css"
+  },
+  {
+    "revision": "d6f57069f567f539ddd8",
+    "url": "/shackletons/static/css/main.ce07b8f0.chunk.css"
+  },
+  {
+    "revision": "fa2be50913fcb64cc5af",
+    "url": "/shackletons/static/js/2.2215dafa.chunk.js"
+  },
+  {
+    "revision": "fd9ecfeee8ee0b17f952",
+    "url": "/shackletons/static/js/3.2ac3b00e.chunk.js"
+  },
+  {
+    "revision": "4d2f5309c156143fdedd",
+    "url": "/shackletons/static/js/4.c9c5b804.chunk.js"
+  },
+  {
+    "revision": "d0ab2d9dd0709d6709ed",
+    "url": "/shackletons/static/js/5.6a558811.chunk.js"
+  },
+  {
+    "revision": "91eeb61308f2a3b40a55",
+    "url": "/shackletons/static/js/6.7e1f9072.chunk.js"
+  },
+  {
+    "revision": "d6f57069f567f539ddd8",
+    "url": "/shackletons/static/js/main.22e1b32b.chunk.js"
+  },
+  {
+    "revision": "b3047fcae0af4edfe154",
+    "url": "/shackletons/static/js/runtime-main.fb5722fc.js"
+  },
+  {
+    "revision": "88714a27c1a6c90148b5793a4b8ad8cb",
+    "url": "/shackletons/static/media/loading.88714a27.gif"
+  },
+  {
+    "revision": "85f06b11051cf84df9f06677d7135771",
+    "url": "/shackletons/static/media/shackletons-edited.85f06b11.png"
+  }
+]);
